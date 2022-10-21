8998dfbbc8e8609443038b0ae8347d2dab906b69
Allan - XMII <allan1507@outlook.com>
2022-10-20 22:01:11 -0300

    criado manual

--- views/manual/manual.js
+++ views/manual/manual.js
@@ -1,3 +1,29 @@
+const topicos = ['w3c', 'visu', 'api'];
+
 export function init() {
-  console.log('manual');
+  loadEvents();
+  fetch(`./views/manual/topicos/w3c.html`)
+    .then((response) => response.text())
+    .then((html) => {
+      document.querySelector('#topico').innerHTML = html;
+    });
+}
+
+function loadEvents() {
+  [].slice.call(document.querySelectorAll('.btn-topico')).forEach(function (btn) {
+    btn.addEventListener('click', loadTopico);
+  });
+}
+
+function loadTopico() {
+  const id = this.id;
+  const url = `./views/manual/topicos/${id}.html`;
+
+  if (topicos.indexOf(id) === -1) return;
+
+  fetch(url)
+    .then((response) => response.text())
+    .then((html) => {
+      document.querySelector('#topico').innerHTML = html;
+    });
 }
